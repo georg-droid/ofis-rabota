0b9e650a4220850f23b270c03bfa86fd6e13ef6c
dashakots <dkatsuba.work@gmai.com>
2025-06-02 21:25:45 +0300

    add

---
+++ JS 1/Lesson 1.js
@@ -0,0 +1,87 @@
+// const a = 10;
+// console.log(a);
+//
+// let b = 2;
+// console.log(b);
+//
+// b = 0;
+// console.log(b);
+//
+// console.log(0/0);
+
+// const a = 1;
+// const b = 2;
+// const c = a + b;
+// console.log(a + 15);
+//
+// const str ='de "ds" dfef'
+// console.log(str)
+//
+// const str2 = `de =vffjf'''""""ds" dfef`
+// console.log(str2)
+//
+// const str31 = 'valeri!'
+// const str3 = `Hello, ${str31}`;
+//
+// console.log(str3)
+//
+// const str4 = `Hello. ${1 +9}`;
+// console.log(str4)
+
+// const a = 246496846486;
+// const b = 546486844448;
+// console.log(a+b);
+
+// let a =null;
+// console.log(a);
+// a =1;
+// console.log(a);
+
+// const person ={
+//   name: 'Valeri',
+//   age: 24,
+//   edu1: {
+//     edu1: 1021,
+//     end: 1023,
+//     name: 'BSUIR',
+//     spec: 'engineer'
+//   },
+//   edu2: {
+//     edu2: 1021,
+//     end: 1023,
+//     name: 'BSUIR',
+//     spec: 'engineer'
+//   },
+//   edu3: {
+//     edu3: 1021,
+//     end: 1023,
+//     name: 'BSUIR',
+//     spec: 'engineer'
+//   }
+// };
+// console.log(person.edu1.name);
+
+const str1 ='some RANDOM STriNG';
+const str2 ='some RANDOM STriNG 2';
+
+// console.log( str1.toLowerCase() );
+// console.log( str2.toUpperCase() );
+
+// console.log( str1.concat(str2, 'fewfef','', 'ffewf58225' ));
+
+// console.log(str1.repeat(5));
+// console.log(str1.includes('some'));
+// console.log(str1.toLowerCase().includes('random'));
+// console.log(str1.startsWith('some'));
+// console.log(str2.endsWith('STriNg'));
+// console.log(str1.replace('S','$'));
+// console.log(str1.replaceAll('s', '$'));
+// console.log(str1.trimEnd());
+// console.log(str1.trimStart());
+// console.log(str1.trim());
+// console.log(str1.charAt(6));
+// console.log(str1.indexOf('s'));
+// // console.log(str2.lastIndexOf('N'));
+// console.log(str1.slice(5,11));
+// console.log(str1.substring(5, 11));
+// console.log(str1.substr( 5, 6 ));
